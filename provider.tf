9645dbd9e8ac9638b0e19b50506a817073a5f309
manu-207 <117650246+manu-207@users.noreply.github.com>
2023-01-03 12:10:56 +0530

    changes new

--- provider.tf
+++ provider.tf
@@ -3,10 +3,8 @@ provider "aws" {
 }
 resource "aws_vpc" "main" {
   cidr_block                = "${var.vpc_cidr}"
-  enable_dns_hostnames      = "${var.enable_hostnames}"
-  enable_dns_support        = "${var.enable_dns}"
-
-  tags {
+  
+   tags {
     Name                    = var.vpc_name
   }
 }
--- vars.tf
+++ vars.tf
@@ -6,14 +6,6 @@ variable "vpc_cidr" {
  
   description                               = "CIDR for the VPC"
 }
-variable "enable_hostnames" {
-  
-  description                               = "enable/disable DNS hostnames in the VPC"
-}
-variable "enable_dns" {
- 
-  description                               = "enable/disable DNS support in the VPC"
-}
 variable "region" {
  
   description                               = "this the region belongs to "
